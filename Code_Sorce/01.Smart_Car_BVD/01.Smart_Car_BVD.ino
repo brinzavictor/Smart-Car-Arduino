b6f38cacca084044ed816bd44c5b912f62028937
Brinza Victor-Daniel <brinza_victor@yahoo.com>
2020-09-26 20:34:47 +0300

    Edit Code direction function

--- Code_Sorce/01.Smart_Car_BVD/01.Smart_Car_BVD.ino
+++ Code_Sorce/01.Smart_Car_BVD/01.Smart_Car_BVD.ino
@@ -4,10 +4,12 @@
 
 /* Define ----------------------------------------------*/
 /* DC Motors pins */
-#define M_STANGA_F D5
-#define M_STANGA_B D6
-#define M_DREAPTA_F D7
-#define M_DREAPTA_B D8
+#define M_LEFT_F D5
+#define M_LEFT_B D6
+#define M_RIGHT_F D7
+#define M_RIGHT_B D8
+#define CORRECTION_SPEED 20
+#define TURN_COEFFICIENT 60
 
 /* Setup -----------------------------------------------*/
 /* Set i2c address */
@@ -19,10 +21,10 @@ PCF8574 pcf8574(0x39);
 void setup() 
 {
     /* Set DC Motors Pins */
-    pinMode(M_STANGA_F, OUTPUT);
-    pinMode(M_STANGA_B, OUTPUT);
-    pinMode(M_DREAPTA_F, OUTPUT);
-    pinMode(M_DREAPTA_B, OUTPUT);
+    pinMode(M_LEFT_F, OUTPUT);
+    pinMode(M_LEFT_B, OUTPUT);
+    pinMode(M_RIGHT_F, OUTPUT);
+    pinMode(M_RIGHT_B, OUTPUT);
 
     /* Set Pins I/O I2C */
     pcf8574.pinMode(P0, OUTPUT);
@@ -38,10 +40,92 @@ void loop()
     /* Ex I2C Pins
     pcf8574.digitalWrite(P0, HIGH);
     */
-    analogWrite(M_DREAPTA_F, 1023);
-    analogWrite(M_STANGA_F, 1023);
-    delay(5000);
-    analogWrite(M_DREAPTA_F, 0);
-    analogWrite(M_STANGA_F, 0);
-    delay(5000);
+}
+
+void go(byte direction, unsigned short speed)
+{
+    switch(direction)
+    {
+         case 0:
+        {
+            // Standby
+            digitalWrite(M_LEFT_F, LOW);
+            digitalWrite(M_LEFT_B, LOW);
+            digitalWrite(M_RIGHT_F, LOW);
+            digitalWrite(M_RIGHT_B, LOW);
+                     
+        }
+        break;
+        
+        case 1:
+        {         
+            // STOP
+            digitalWrite(M_LEFT_F, HIGH);
+            digitalWrite(M_LEFT_B, HIGH);
+            digitalWrite(M_RIGHT_F, HIGH);
+            digitalWrite(M_RIGHT_B, HIGH);           
+        }
+        break;
+
+        case 2:
+        {
+            // In fata la viteza
+            analogWrite(M_LEFT_F, speed - CORRECTION_SPEED);
+            digitalWrite(M_LEFT_B, LOW);
+            analogWrite(M_RIGHT_F, speed);
+            digitalWrite(M_RIGHT_B, LOW);      
+        }
+        break;
+
+        case 3:
+        {
+            // In spate la viteza
+            digitalWrite(M_LEFT_F, LOW);
+            analogWrite(M_LEFT_B, speed - CORRECTION_SPEED);
+            digitalWrite(M_RIGHT_F, LOW);
+            analogWrite(M_RIGHT_B, speed);         
+        }
+        break;
+
+        case 4:
+        {
+              // Rotire Stanga
+            digitalWrite(M_LEFT_F, LOW);
+            digitalWrite(M_LEFT_B, HIGH);
+            digitalWrite(M_RIGHT_F, HIGH);
+            digitalWrite(M_RIGHT_B, LOW);         
+        }
+        break;
+        
+        case 5:
+        {
+            // Rotire Dreapta
+            digitalWrite(M_LEFT_F, HIGH);
+            digitalWrite(M_LEFT_B, LOW);
+            digitalWrite(M_RIGHT_F, LOW);
+            digitalWrite(M_RIGHT_B, HIGH);  
+                       
+        }
+        break;
+
+        case 6:
+        {
+            // Vireaza stanga
+            analogWrite(M_LEFT_F, speed - TURN_COEFFICIENT);
+            digitalWrite(M_LEFT_B, LOW);
+            analogWrite(M_RIGHT_F, speed);
+            digitalWrite(M_RIGHT_B, LOW);
+        }
+        break;
+
+        case 7:
+        {
+            //Vireaza dreapta
+            analogWrite(M_LEFT_F, speed);
+            digitalWrite(M_LEFT_B, LOW);
+            analogWrite(M_RIGHT_F, speed - TURN_COEFFICIENT);
+            digitalWrite(M_RIGHT_B, LOW);          
+        }
+        break;
+    }
 }
